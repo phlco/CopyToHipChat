d56a3425d8d32542ef871d0c605c876273258b36
phlco <phillip.lamplugh@gmail.com>
2014-01-30 17:01:54 -0500

    comments out file name based on language

--- copy_to_hipchat.py
+++ copy_to_hipchat.py
@@ -1,23 +1,51 @@
-# in hipchat /code Displays message with code syntax highlighting
-# this sets current text selection to the clipboard and appends `/code `
+# in hipchat prefacing `/code` displays message with syntax highlighting
+# this sets current text selection to the clipboard
+# appends `/code ` and the commented filename
 
 import sublime, sublime_plugin
 
 class CopyToHipChatCommand(sublime_plugin.TextCommand):
-  def run(self, edit):
+    def run(self, edit):
 
-    # get highlighted text
-    sel = self.view.sel()
-    region1 = sel[0]
-    selectionText = self.view.substr(region1)
+        # get highlighted text
+        sel = self.view.sel()
+        region1 = sel[0]
+        selectedText = self.view.substr(region1)
 
-    # get the syntax
-    syntax = self.view.settings().get('syntax')
+        # get file name as a commented out string
+        commented_filename = self.comment_file_name(edit)
 
-    # get file path
-    file_name = self.view.file_name()
-    # comment style
-    comment_style = "#"
-    # append `/code `
-    clip_data = "/code " + comment_style + " " + file_name + "\n" + selectionText
-    sublime.set_clipboard(clip_data)
+        # append `/code `
+        clip_data = "/code " + commented_filename + "\n" + selectedText
+
+        # copy to clipboard
+        sublime.set_clipboard(clip_data)
+
+
+    def comment_file_name(self, edit):
+
+        # get file_name
+        file_name = self.view.file_name() + "\n"
+        # add file_name to top of file
+        self.view.insert(edit, 0, file_name)
+        # select the file name
+        selection = self.view.full_line(0)
+        selectionText = self.view.substr(selection)
+
+        # move cursor to top of page
+        target = self.view.text_point(0, 0)
+        self.view.sel().clear()
+        # select the first line
+        self.view.sel().add(sublime.Region(target))
+
+        # comment out the filename
+        self.view.run_command("toggle_comment")
+
+        # get commented out filename
+        selection = self.view.full_line(0)
+        commented_out_file_name = self.view.substr(selection).rstrip()
+
+        # remove commented_file_name
+        self.view.erase(edit, selection)
+
+        return commented_out_file_name
