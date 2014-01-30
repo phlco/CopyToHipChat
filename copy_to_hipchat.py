d53066d64ed3fe1576babaf07840c9fab069fcf7
phlco <phillip.lamplugh@gmail.com>
2014-01-30 14:03:10 -0500

    adds commented file path to top

--- copy_to_hipchat.py
+++ copy_to_hipchat.py
@@ -5,7 +5,17 @@ import sublime, sublime_plugin
 
 class CopyToHipChatCommand(sublime_plugin.TextCommand):
   def run(self, edit):
+
+    # get highlighted text
     sel = self.view.sel()
     region1 = sel[0]
     selectionText = self.view.substr(region1)
-    sublime.set_clipboard( "/code " + selectionText )
+
+    # get the syntax
+    syntax = self.view.settings().get('syntax')
+    # get file path
+    filename = self.view.file_name()
+    # comment out filename
+    filename = "# " + filename + "\n"
+    # append `/code `
+    sublime.set_clipboard( "/code " + filename + selectionText )
