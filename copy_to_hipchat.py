acd7181c9f2281f9274e3d1f0b23bbdc8c558fcc
phlco <phillip.lamplugh@gmail.com>
2014-01-31 09:36:29 -0500

    clean up

--- copy_to_hipchat.py
+++ copy_to_hipchat.py
@@ -1,6 +1,6 @@
 # in hipchat prefacing `/code` displays message with syntax highlighting
 # this sets current text selection to the clipboard
-# appends `/code ` and the commented filename
+# appends `/code ` and the commented file name
 
 import sublime, sublime_plugin
 
@@ -8,15 +8,13 @@ class CopyToHipChatCommand(sublime_plugin.TextCommand):
     def run(self, edit):
 
         # get highlighted text
-        sel = self.view.sel()
-        region1 = sel[0]
-        selectedText = self.view.substr(region1)
+        selected_text = self.get_text_selection(edit)
 
         # get file name as a commented out string
-        commented_filename = self.comment_file_name(edit)
+        commented_file_name = self.comment_file_name(edit)
 
         # append `/code `
-        clip_data = "/code " + commented_filename + "\n" + selectedText
+        clip_data = "/code " + commented_file_name + "\n" + selected_text
 
         # copy to clipboard
         sublime.set_clipboard(clip_data)
@@ -24,28 +22,36 @@ class CopyToHipChatCommand(sublime_plugin.TextCommand):
 
     def comment_file_name(self, edit):
 
-        # get file_name
+        # get the file name
         file_name = self.view.file_name() + "\n"
-        # add file_name to top of file
+
+        # add the file name to top of file
         self.view.insert(edit, 0, file_name)
+
         # select the file name
         selection = self.view.full_line(0)
         selectionText = self.view.substr(selection)
 
         # move cursor to top of page
         target = self.view.text_point(0, 0)
+
         self.view.sel().clear()
         # select the first line
         self.view.sel().add(sublime.Region(target))
 
-        # comment out the filename
+        # comment out the file name
         self.view.run_command("toggle_comment")
 
-        # get commented out filename
+        # get commented out file name
         selection = self.view.full_line(0)
         commented_out_file_name = self.view.substr(selection).rstrip()
 
-        # remove commented_file_name
+        # remove the commented file name
         self.view.erase(edit, selection)
 
         return commented_out_file_name
+
+    def get_text_selection(self, edit):
+        selection = self.view.sel()[0]
+        text_selection = self.view.substr(selection)
+        return text_selection
