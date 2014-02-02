c151995b883845e2e99e575291964265b20b61ac
phlco <phillip.lamplugh@gmail.com>
2014-02-02 12:13:38 -0500

    makes file_name optional

--- copy_to_hipchat.py
+++ copy_to_hipchat.py
@@ -1,40 +1,54 @@
 # -*- coding: utf-8 -*-
 
-# in hipchat prefacing `/code` displays message with syntax highlighting
-# this sets current text selection to the clipboard
-# appends `/code ` and the commented file name
-
 import sublime
 import sublime_plugin
+import os
+
+# #################
+# Copy To HipChat
+# #################
+
+# In HipChat prefacing a message with `/code` displays the message with code syntax highlighting
+# This plugin sets the current text selection in Sublime to the clipboard
+# and appends `/code ` with an optional commented file name and path
 
 class CopyToHipChatCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        # load settings
-        settings = sublime.load_settings('copy_to_hipchat.sublime-settings')
 
         # get highlighted text
         selected_text = self.get_text_selection(edit)
 
-        # get file name as a commented out string
-        if settings.get('include_file_name'):
-            commented_file_name = self.comment_file_name(edit)
-        else:
-            commented_file_name = ""
+        # get optional file_name and path
+        file_name = self.get_file_name(edit)
 
-        # append `/code `
-        clip_data = "/code " + commented_file_name + "\n" + selected_text
+        # build slash_comment
+        slash_comment = "/code "
+        if len(file_name):
+            slash_comment += self.comment_file_name(edit, file_name)
+        slash_comment += "\n"
 
         # copy to clipboard
+        clip_data = slash_comment + selected_text
         sublime.set_clipboard(clip_data)
 
+    def get_text_selection(self, edit):
+        sel = self.view.sel()[0]
+        txt = self.view.substr(sel)
+        return txt
 
-    def comment_file_name(self, edit):
+    def get_file_name(self, edit):
+        path, basename = os.path.split(self.view.file_name())
+        settings = sublime.load_settings("copy_to_hipchat.sublime-settings")
+        file_name = ""
+        if settings.get("path"):
+            file_name += path + "/"
+        if settings.get("file_name"):
+            file_name += basename
+        return file_name
 
-        # get the file name
-        file_name = self.view.file_name() + "\n"
-
-        # add the file name to top of file
-        self.view.insert(edit, 0, file_name)
+    def comment_file_name(self, edit, file_name):
+        # add the file name to top of file with a new line
+        self.view.insert(edit, 0, file_name + "\n")
 
         # select the file name
         selection = self.view.full_line(0)
@@ -59,7 +73,3 @@ class CopyToHipChatCommand(sublime_plugin.TextCommand):
 
         return commented_out_file_name
 
-    def get_text_selection(self, edit):
-        selection = self.view.sel()[0]
-        text_selection = self.view.substr(selection)
-        return text_selection
