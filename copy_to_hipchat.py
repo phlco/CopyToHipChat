01b7c43498c9c6a3de05fa67a7da43f15889f29b
phlco <phillip.lamplugh@gmail.com>
2014-02-04 23:42:59 -0500

    rewrite

--- copy_to_hipchat.py
+++ copy_to_hipchat.py
@@ -1,86 +1,51 @@
-# -*- coding: utf-8 -*-
-
 import sublime
 import sublime_plugin
 import os
 
-# #################
-# Copy To HipChat
-# #################
-
-# In HipChat prefacing a message with `/code` displays the message with code syntax highlighting
-# This plugin sets the current text selection in Sublime to the clipboard
-# and appends `/code ` with an optional commented file name and path
+settings = sublime.load_settings("copy_to_hipchat.sublime-settings")
 
 class CopyToHipChatCommand(sublime_plugin.TextCommand):
-    def run(self, edit):
 
-        # get highlighted text
-        selected_text = self.get_text_selection(edit)
+  global settings
 
-        # get optional file_name and path
-        file_name = self.get_file_name(edit)
+  def run(self, edit):
+    sublime.status_message("copying to hipchat")
 
-        # build slash_comment
-        slash_comment = "/code "
-        if len(file_name):
-            slash_comment += self.comment_file_name(edit, file_name)
-        slash_comment += "\n"
+    slash = "/code "
+    comment = self.get_comment(edit)
+    message = self.view.substr(self.view.sel()[0])
 
-        # copy to clipboard
-        clip_data = slash_comment + selected_text
-        sublime.set_clipboard(clip_data)
+    sublime.set_clipboard(slash + comment + "\n" + message)
 
-    def get_text_selection(self, edit):
-        sel = self.view.sel()[0]
-        txt = self.view.substr(sel)
-        return txt
+  def get_comment(self, edit):
+    file_name = self.get_file_name()
+    scratchBuffer = self.get_buffer()
+    scratchBuffer.insert(edit, 0, file_name)
+    scratchBuffer.run_command("select_all")
+    scratchBuffer.run_command("toggle_comment")
+    selection = scratchBuffer.full_line(0)
+    commented_text = scratchBuffer.substr(selection).rstrip()
+    scratchBuffer.close()
+    return commented_text
 
-    def get_file_name(self, edit):
-        if self.view.file_name():
-            path, basename = os.path.split(self.view.file_name())
-            settings = sublime.load_settings("copy_to_hipchat.sublime-settings")
-            file_name = ""
-            if settings.get("path"):
-                file_name += path + "/"
-            if settings.get("file_name"):
-                file_name += basename
-            return file_name
-        else:
-            # todo: add filetype?
-            # scope = self.view.scope_name(self.view.sel()[0].a)
-            return "unsaved file"
+  def get_file_name(self):
+    file_name = ""
+    if self.view.file_name() == None:
+      file_name += "Unsaved"
+    else:
+      path, file = os.path.split(self.view.file_name())
+      if settings.get("path"):
+        file_name += path
+      if settings.get("file"):
+        file_name += file
+    return file_name
 
-    def comment_file_name(self, edit, file_name):
-        # add the file name to top of file with a new line
-        self.view.insert(edit, 0, file_name + "\n")
+  def get_buffer(self):
+    sublime.status_message("making scratch buffer")
 
-        # select the file name
-        selection = self.view.full_line(0)
-        selectionText = self.view.substr(selection)
-
-        # move cursor to top of page
-        target = self.view.text_point(0, 0)
-
-        self.view.sel().clear()
-        # select the first line
-        self.view.sel().add(sublime.Region(target))
-
-        # comment out the file name
-        settings = sublime.load_settings("copy_to_hipchat.sublime-settings")
-        if settings.get("native_comment"):
-            self.view.run_command("toggle_comment")
-
-        # get commented out file name
-        selection = self.view.full_line(0)
-        commented_out_file_name = self.view.substr(selection).rstrip()
-
-        if not settings.get("native_comment"):
-            garnish = settings.get("garnish")
-            commented_out_file_name = garnish + commented_out_file_name
-
-        # remove the commented file name
-        self.view.erase(edit, selection)
-
-        return commented_out_file_name
-
+    new_buffer = sublime.Window.new_file(self.view.window())
+    new_buffer.set_name("Scratch")
+    new_buffer.set_scratch(True)
+    syntax = self.view.settings().get('syntax')
+    new_buffer.set_syntax_file(syntax)
+    return new_buffer
